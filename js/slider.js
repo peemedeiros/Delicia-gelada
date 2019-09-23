88121f581782bb5dd9d39e2a31245c267758e508
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-09-23 01:11:36 -0300

    atualizando js do slider

--- js/slider.js
+++ js/slider.js
@@ -24,13 +24,3 @@ function MudarSlide(pos) {
 	var slidewidth = document.getElementById("slideshow").offsetWidth
 	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
 }
-function toggleMenu() {
-
-	var menu = document.getElementById("menu");
-
-	if (menu.style.display == 'none' || menu.style.display == '') {
-		menu.style.display = "block";
-	} else {
-		menu.style.display = "none"
-	}
-}
