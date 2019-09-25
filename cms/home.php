61bc2c6c13a050f6d4558921921ed4e2c3358484
Pedro Medeiros <pedrohenriquelm500@gmail.com>
2019-09-25 16:14:15 -0300

    criando o CMS

---
+++ cms/home.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <title>
+            CMS
+        </title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="../css/style.css">
+    </head>
+    <body>
+        <section id="cms">
+            <div class="conteudo center">
+                <div class="cms-estrutura">
+                    <div class="cms-header">
+                        <h1 class="titulo">
+                            CMS
+                        </h1>
+                        <h3 class="sub-titulo">
+                            - Sistema de Gerenciamento do Site
+                        </h3>
+                        <div class="cms-img">
+                            <img src="../icon/icon-logo-barra.png" alt="logo">
+                        </div>
+                    </div>
+                    <nav class="cms-nav">
+                        <div class="cms-itens">
+                            <div class="cms-itens-imagens center">
+                                <img src="../icon/execution.png" alt="adm">
+                            </div>
+                            Adm. Conteúdo
+                        </div>
+                        <div class="cms-itens">
+                            <div class="cms-itens-imagens center">
+                                <img src="../icon/consumer.png" alt="adm">
+                            </div>
+                            Adm. Fale Conosco
+                        </div>
+                        <div class="cms-itens">
+                            <div class="cms-itens-imagens center">
+                                <img src="../icon/multiple-users-silhouette.png" alt="adm">
+                            </div>
+                            Adm. Usuários
+                        </div>
+                        <div class="cms-itens float-right">
+                            <div class="texto">Bem vindo, [xxxxxxxxxx].</div>
+                            <div class="bnt-log-out">
+                                log out
+                            </div>
+                        </div>
+                    </nav>
+                    <div class="cms-conteudo">
+                    
+                    </div>
+                    <div class="cms-footer">
+                        
+                    </div>
+                </div>
+            </div>
+        </section> 
+    </body>
+</html>
